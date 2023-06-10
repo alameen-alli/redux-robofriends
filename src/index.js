755b5084a5a1f6820544a0e925f01cf63fe7dd6e
unknown <allialameen8739@gmail.com>
2023-06-10 14:31:41 +0100

    state prop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore} from 'redux';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots } from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(searchRobots);
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App store={store} />);
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
